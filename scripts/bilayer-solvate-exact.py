ea3d383f5332fbb5998d9211e13d72ff78f562d4
Ryan Bradley <bradleyrp@gmail.com>
2018-09-19 23:00:36 -0400

    developing replicates

--- scripts/bilayer-solvate-exact.py
+++ scripts/bilayer-solvate-exact.py
@@ -19,9 +19,49 @@ solvate(
 water_solvate_exact('solvate-big.gro','solvate.gro')
 structure = GMXStructure(state.here+'solvate.gro')
 state.composition = structure.detect_composition()
+
 write_top('solvate.top')
 minimize('solvate',restraints=True)
 remove_jump(
 	structure='solvate-minimized',
 	tpr='em-solvate-steep',
 	gro='solvate-nojump')
+
+remove_jump(
+	structure='solvate-minimized',
+	tpr='em-solvate-steep',
+	gro='solvate-nojump')
+
+counterions('solvate-nojump','solvate',restraints=True,
+	counts=state.genion_overrides)
+
+counterion_renamer('counterions')
+write_top('counterions.top')
+minimize('counterions',restraints=True)
+remove_jump(
+	structure='counterions-minimized',
+	tpr='em-counterions-steep',
+	gro='counterions-nojump')
+bilayer_middle(
+	structure='counterions-nojump',
+	gro='system')
+bilayer_sorter(
+	structure='system',
+	ndx='system-groups')
+# relax with fixed area and upright restraints
+write_top('system.top')
+# note no persistent restraint positions
+equilibrate(
+	seq=state.equilibrate_restrain,
+	restraints=True,
+	groups='system-groups',
+	stages_only=True)
+# release restraints and continue
+state.force_field = settings.force_field
+move_file('system.top','system-restrain.top')
+write_top('system.top')
+equilibrate(
+	structure=state.equilibrate_restrain_final,
+	seq=state.equilibrate_free,
+	groups='system-groups',
+	stages_only=False)
